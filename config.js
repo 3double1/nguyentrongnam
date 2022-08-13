cd96c54684b968192b39c45f4f6f7e8ca0448948
3double1 <89219120+3double1@users.noreply.github.com>
2022-08-14 01:51:33 +0700

    Add files via upload
    happy

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'BẠN';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'HAPPY BIRTHDAY';
+const titleCard = 'Tặng Nguyễn Phương Trang';
+const contentCard = 'Chúc mừng sinh nhật bạn ❤️, chúc bạn tuổi mới sẽ có thật nhiều niềm vui, hạnh phúc và may mắn 🍀 chúc bạn luôn luôn có một sức khỏe thật dồi dào nè, và chúc mừng bạn đã già thêm một tuổi 😆 Lò vé ❤️ Và sẽ sớm có được tui 😘🥰';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'NY.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
